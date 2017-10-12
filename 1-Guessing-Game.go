627de9526b07f4411bb628d61a65a94c41e8b4ce
Kevin Barry <g00339811@gmit.ie>
2017-10-12 22:20:00 +0100

    first commit

---
+++ 1-Guessing-Game.go
@@ -0,0 +1,25 @@
+// Problem set - web application.
+// Problem 1 - Guessing Game
+// kevin barry 12-10-17
+// https://data-representation.github.io/problems/go-web-applications.html
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+//	"bytes"
+)
+
+func requestHandler(w http.ResponseWriter, r *http.Request) {
+
+
+	w.Header().Set("Content-Type","text/html");// allows browser to render html tags
+
+	fmt.Fprintln(w, "Guessing Game")
+}
+
+func main() {
+	http.HandleFunc("/", requestHandler)
+	http.ListenAndServe(":8080", nil)
+}
