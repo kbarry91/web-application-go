d577b8226774a62ea56c7068c27df25832ba658b
Kevin Barry <g00339811@gmit.ie>
2017-10-18 10:50:40 +0100

    template working and passing data

--- 2-h1-text.go
+++ 2-h1-text.go
@@ -6,21 +6,30 @@
 package main
 
 import (
-	
+	"html/template"//add html/template package 
 	"net/http"
 //	"bytes"
 )
+	
+type myMsg struct {
+    Message string
+}
 
 func requestHandler(w http.ResponseWriter, r *http.Request) {
-
 //serve a html file instead of hardcoded html
 	http.ServeFile(w, r, "index.html")
 }
 
 func guessHandler(w http.ResponseWriter, r *http.Request) {
+
+	//create and initialise string
+	message :="Guess a number between 1 and 20"
+
+	//read the contents og guess.html and return a template
+	t, _ := template.ParseFiles("guess.tmpl")
 	
-	//serve a html file instead of hardcoded html
-		http.ServeFile(w, r, "guess.html")
+	//execute template and pass pointer to myMsg struct
+	t.Execute(w, &myMsg{Message:message})
 }//guessHandler
 
 func main() {
