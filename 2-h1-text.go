c158a5aaf64c3ee86ab3aaf4c1f48c063f827d4f
Kevin Barry <g00339811@gmit.ie>
2017-10-19 23:23:07 +0100

    part5/6 cookie set needs edit 0value

--- 2-h1-text.go
+++ 2-h1-text.go
@@ -8,6 +8,10 @@ package main
 import (
 	"html/template"//add html/template package 
 	"net/http"
+	"math/rand"//imports math random package
+	"time"
+	"strconv"
+
 //	"bytes"
 )
 	
@@ -25,7 +29,32 @@ func guessHandler(w http.ResponseWriter, r *http.Request) {
 	//create and initialise string
 	message :="Guess a number between 1 and 20"
 
-	//read the contents og guess.html and return a template
+	//set up a seed for random number generator
+	//https://stackoverflow.com/questions/12321133/golang-random-number-generator-how-to-seed-properly
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	target:=0//added to delete undefined issue
+	var cookie, err = r.Cookie("target")//gets cookie called count
+
+	if err == nil{
+		//if we could read it ,try to convert its value to an int
+		target, _ = strconv.Atoi(cookie.Value)
+		if target ==0{
+			target = rand.Intn(20-1)
+		}
+	}
+
+	//set cookie details at pointer address to &http.cookie
+	cookie = &http.Cookie{
+		Name: "target",
+		Value: strconv.Itoa(target),
+		Expires: time.Now().Add(72 * time.Hour),
+	}
+	
+	//set the cookie
+	http.SetCookie(w,cookie)
+	
+	//read the contents of guess.html and return a template
 	t, _ := template.ParseFiles("guess.tmpl")
 	
 	//execute template and pass pointer to myMsg struct
